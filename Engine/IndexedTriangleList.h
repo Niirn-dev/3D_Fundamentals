275aa2bf197671d6e372a5fb193c677fdd989cf6
Niirn <niirn-dev@users.noreply.github.com>
2020-09-13 20:12:26 +0300

    added option to concatenate indexed triangle list if I ever need it

--- Engine/IndexedTriangleList.h
+++ Engine/IndexedTriangleList.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include "Vec3.h"
 #include <cassert>
+#include <algorithm>
+#include <iterator>
 
 template<typename T>
 class IndexedTriangleList
@@ -15,6 +17,22 @@ public:
 	{
 		assert( this->vertices.size() > 2 );
 		assert( this->indices.size() % 3 == 0 );
+	}
+	IndexedTriangleList& operator+=( const IndexedTriangleList& rhs )
+	{
+		const size_t nVerts = vertices.size();
+		std::copy( rhs.vertices.begin(),rhs.vertices.end(),std::back_inserter( vertices ) );
+		std::transform( rhs.indices.begin(),rhs.indices.end(),
+						std::back_inserter( indices ),
+						[&nVerts]( size_t i )
+						{
+							return i + nVerts;
+						} );
+		return *this;
+	}
+	IndexedTriangleList operator+( const IndexedTriangleList& rhs ) const
+	{
+		return IndexedTriangleList( *this ) += rhs;
 	}
 
 	std::vector<T> vertices;
